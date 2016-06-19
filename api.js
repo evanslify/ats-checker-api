54c39055efb0ff7a17085feac46b2c522f413537
es <es@evsfy.com>
2016-06-19 18:03:03 +0800

    fuck heroku

--- api.js
+++ api.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 var https = require('https');
-var http = require('http');
 var constants = require('constants');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -17,25 +16,14 @@ var options = {
 
 
 app.use(bodyParser.json());
-app.get('/', function(req, res){
-    res.send('1');
-});
 app.post('/atscheck/', function(req, res){
     const target = req.body.url;
     check.startCheck(target, res);
 });
 
-// heroku says: thou shall not decide which port to use.
-var port = process.env.PORT || 4430;
-// https.createServer(options, app).listen(port, function(){
-    // console.log("Express server listening on port " + port);
-// });
 
-const server = http.createServer((req, res) => {
-    res.send('2');
-  res.end();
+var port = 443;
+https.createServer(options, app).listen(port, function(){
+    console.log("Express server listening on port " + port);
 });
-server.on('clientError', (err, socket) => {
-  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-});
-server.listen(port);
+
